45ab60802469891bc5b030c7d2de373d07759a96
Pratik Kumar Sharma <pratik.kumar.sharma@aon.com>
2026-04-20 10:48:00 +0530

    Adding Backend Walking skelton with working endpoints

---
+++ API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Adding DB configuration for the API
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
+
+var app = builder.Build();
+
+// Configure middleware pipeline
+app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+} catch(Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An Error Occurred During Migration.");
+}
+
+
+app.Run();
---
+++ Domain/Activity.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public required string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public required string Description { get; set; }
+
+        public required string Category { get; set; }
+
+        public bool IsCancelled { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Venue { get; set; }
+
+        public required string Latitude { get; set; }
+
+        public required string Longitude { get; set; }
+
+    }
+}
